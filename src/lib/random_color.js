ead3b66ab6f1c9cef5c6c0523b53167e78d232c0
Victor Schweitz <victorschweitz@hotmail.com>
2024-05-31 21:55:21 +0200

    Note done!

---
+++ src/lib/random_color.js
@@ -0,0 +1,10 @@
+// const note = document.querySelector('.note')
+    
+// export function generateRandomNoteStyle()
+// {
+//     const noteStyles = ['one', 'two', 'three', 'four']
+//     const randomNoteStyle = noteStyles[Math.floor(Math.random() * noteStyles.length)]
+//     return note.classList.add(randomNoteStyle)
+// }
+
+// generateRandomNoteStyle()
